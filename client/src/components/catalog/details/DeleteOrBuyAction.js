70a9b9cbc4e4d63e7145b968e791018f43bf68a4
Емил Стойчев <estallinone@gmail.com>
2022-07-28 19:15:41 +0300

    make buy handler

--- client/src/components/catalog/details/DeleteOrBuyAction.js
+++ client/src/components/catalog/details/DeleteOrBuyAction.js
@@ -3,7 +3,7 @@ import * as productService from '../../../services/catalog/productService'
 import * as authService from '../../../services/user/authService'
 import { useNavigate } from "react-router-dom";
 
-export const DeleteOrBuyAction = ({onDeleteClickHandler, product, options}) => {
+export const DeleteOrBuyAction = ({ onDeleteClickHandler, product, options , setProduct, setOptions, setCookies}) => {
     let navigate = useNavigate()
 
     const deleteHandler = () => {
@@ -11,13 +11,13 @@ export const DeleteOrBuyAction = ({onDeleteClickHandler, product, options}) => {
 
         productService.deleteProduct(product._id, cookie)
             .then(result => {
-                if(result.message) {
+                if (result.message) {
                     console.log(result);
                     navigate('/404')
                 } else {
                     authService.deleteProductFromUser(cookie, product._id)
                         .then(result => {
-                            if(result.message) {
+                            if (result.message) {
                                 console.log(result);
                                 navigate('/404')
                             } else {
@@ -29,7 +29,29 @@ export const DeleteOrBuyAction = ({onDeleteClickHandler, product, options}) => {
     }
 
     const buyHandler = () => {
-        console.log('clicked');
+        let cookie = getCookie('sessionStorage')
+
+        productService.changeProductAuthor(cookie, product._id)
+            .then(result => {
+                if (result.message) {
+                    console.log(result);
+                } else {
+                    let updatedProduct = result
+                    authService.updateUserAfterBuy(cookie, product)
+                        .then(result => {
+                            if (result.message) {
+                                console.log(result);
+                            } else {
+                                updatedProduct.email = cookie.email
+                                cookie.money = Number(cookie.money) - Number(updatedProduct.price)
+
+                                setCookies(cookie)
+                                setOptions({ type: '', action: false })
+                                setProduct(updatedProduct)
+                            }
+                        })
+                }
+            })
     }
 
     return (
@@ -40,8 +62,8 @@ export const DeleteOrBuyAction = ({onDeleteClickHandler, product, options}) => {
                     <div className="confirm-container">
                         <header className="headers">
                             <h2>Are you sure you want to {options.type === 'buy' ? 'buy' : 'delete'} this item?</h2>
-                                <button id="action-save" className="btn btn-primary" type="submit" style={{margin: "0 1% 0 0"}} onClick={options.type === 'buy' ? buyHandler : deleteHandler}> {options.type === 'buy' ? 'Buy' : 'Delete'} </button>
-                                <button id="action-cancel" className="btn btn-primary" type="button" onClick={() => onDeleteClickHandler('', false)} > Cancel </button>
+                            <button id="action-save" className="btn btn-primary" type="submit" style={{ margin: "0 1% 0 0" }} onClick={options.type === 'buy' ? buyHandler : deleteHandler}> {options.type === 'buy' ? 'Buy' : 'Delete'} </button>
+                            <button id="action-cancel" className="btn btn-primary" type="button" onClick={() => onDeleteClickHandler('', false)} > Cancel </button>
                         </header>
                     </div>
                 </div>
