19afabe279967c55ece27b3cfd4aa5e90e001803
Емил Стойчев <estallinone@gmail.com>
2022-07-31 16:20:21 +0300

    Make sort for messages

--- client/src/components/catalog/messages/Messages.js
+++ client/src/components/catalog/messages/Messages.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 
 import * as authService from '../../../services/user/authService'
 import getCookie from "../../cookies/getCookie";
@@ -7,22 +6,34 @@ import { MessageTemplate } from "./MessageTemplate";
 
 export const Messages = () => {
     const [messages, setMessages] = useState([])
+    const [type, setType] = useState(false)
     const cookie = getCookie('sessionStorage')
 
     useEffect(() => {
         authService.getMessages(cookie?._id)
-            .then(result => setMessages(result))
+            .then(result => {
+                setMessages(result)
+            })
     }, [])
+
+    const sortByRead = () => {
+        if(type) {
+            setMessages(messages.sort((a,b) => a.read - b.read));
+            setType(!type)
+        } else {
+            setMessages(messages.sort((a,b) => b.read - a.read));
+            setType(!type)
+        }
+    }
 
     return (
         <>
-            <h1 style={{ textAlign: "center", margin: "2% 0 0 0" , fontFamily: "Copperplate Gothic" , userSelect: "none" }}>All messages</h1>
-            <button className="btn btn-primary" style={{ margin: "0 3.2%" }} > Sort by date </button>
-                        <button className="btn btn-primary" style={{ margin: "0 -2.5%" }} > Sort by read </button>
+            <h1 style={{ textAlign: "center", margin: "2% 0 0 0" , fontFamily: "Copperplate Gothic" , userSelect: "none" , color: "navajowhite" }}>All messages</h1>
+            <button className="btn btn-primary" style={{ margin: "0 3.2%" }} onClick={sortByRead}> Sort by read </button>
 
             {messages.length > 0
                 ?
-                <div className="row row-cols-1 row-cols-md-1 g-5" style={{ margin: "0 4%" }} >
+                <div className="row row-cols-1 row-cols-md-1 g-5" style={{ margin: "0 4% 25%" }} >
                     {messages.map(x => <MessageTemplate key={x._id} data={x} cookie={cookie} setMessages={setMessages}/>)}
                 </div>
                 :
