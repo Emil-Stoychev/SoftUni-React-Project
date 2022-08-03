8c099352990f4ebe80e82dfc098bdbf189034448
Емил Стойчев <estallinone@gmail.com>
2022-08-03 18:43:27 +0300

    Delete search hook

--- client/src/hooks/useSearch.js
+++
@@ -1,9 +0,0 @@
-import { useState } from "react"
-
-const useSearch = (defaultValue) => {
-    const [value, setValue] = useState(defaultValue)
-
-    return [value, setValue]
-}
-
-export default useSearch
