d5723cf4e1ef93baf482a4001afeb093caa84f32
Емил Стойчев <estallinone@gmail.com>
2022-08-05 18:57:03 +0300

    Revert "Revert "Add comment model""
    This reverts commit 0ba3aecbd55c844abdae0ca8617e52fb9e3c6ede.

---
+++ server/utils/CommentEngine.js
@@ -0,0 +1,25 @@
+const { Comment } = require('../Models/Comment')
+
+const addCommentToProduct = async (email, title, authorId, productId) => {
+
+
+    let newDate = new Date()
+
+    let date = newDate.toLocaleString()
+
+    let comment = {
+        email,
+        title,
+        authorId,
+        productId,
+        date
+    }
+
+    let newComment = await Comment.create(comment) 
+
+    return newComment
+}
+
+module.exports = {
+    addCommentToProduct
+}
