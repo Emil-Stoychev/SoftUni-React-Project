b7280b78c3fe8ae370b339c0158265cce51ab742
Емил Стойчев <estallinone@gmail.com>
2022-08-05 18:57:50 +0300

    Make server limit for files

--- server/index.js
+++ server/index.js
@@ -19,7 +19,8 @@ mongoose.connect(URL)
 const app = express()
 
 app.use(cookieParser())
-app.use(express.json())
+app.use(express.json({ limit: '100mb' }))
+app.use(express.urlencoded({ extended: true, limit: '100mb' }))
 app.use(cors())
 app.use(router)
 
