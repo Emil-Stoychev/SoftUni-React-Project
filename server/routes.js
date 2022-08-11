3c253fd6b5c32fd95a997436de3dac3061f33061
Емил Стойчев <estallinone@gmail.com>
2022-08-11 10:29:18 +0300

    Remove profile controller and service

--- server/routes.js
+++ server/routes.js
@@ -1,12 +1,10 @@
 const router = require('express').Router()
 
 const catalogController = require('./controllers/catalogController')
-const profileController = require('./controllers/profileController')
 const authController = require('./controllers/authController')
 const errorController = require('./controllers/errorController.js')
 
 router.use('/catalog', catalogController)
-router.use('/profile', profileController)
 router.use('/users', authController)
 
 router.use('*', errorController)
